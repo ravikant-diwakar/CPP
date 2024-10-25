cd51bbaa7ac146af35c4146dc77928faf7c31446
RAVIKANT DIWAKAR <110620635+ravikant-diwakar@users.noreply.github.com>
2024-10-25 14:17:04 +0530

    Add files via upload

---
+++ nearest_multiple_of_10.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <cstdlib> // For std::abs
+
+using namespace std;
+
+class Solution {
+public:
+    string roundToNearest(string str) {
+
+
+     using namespace std;   
+    }
+};
